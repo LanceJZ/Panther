8cd78577efe463899734b0ec1e5f2da3fe977b03
LanceJZ <lance.zimmerman@gmail.com>
2018-02-12 19:59:46 -0800

    Fixed PlaneEntity class

--- Panther/Engine/PlaneEntity.cs
+++ Panther/Engine/PlaneEntity.cs
@@ -7,8 +7,9 @@ using System;
 
 namespace Panther
 {
-    class PlaneEntity : PositionedObject
+    class PlaneEntity : DrawableGameComponent
     {
+        PositionedObject PO;
         Camera TheCamera;
         VertexPositionTexture[] Verts = new VertexPositionTexture[6];
         VertexBuffer PlaneVertexBuffer;
@@ -21,25 +22,58 @@ namespace Panther
 
         public PlaneEntity(Game game, Camera camera) : base(game)
         {
+            PO = new PositionedObject(game);
             TheCamera = camera;
             PlaneBasicEffect = new BasicEffect(game.GraphicsDevice);
+            game.Components.Add(this);
+
         }
 
         public PlaneEntity(Game game, Camera camera, Texture2D texture) : base(game)
         {
+            PO = new PositionedObject(game);
             TheCamera = camera;
             PlaneBasicEffect = new BasicEffect(game.GraphicsDevice);
             XNATexture = texture;
+            game.Components.Add(this);
+        }
+
+        public PlaneEntity(Game game, Camera camera, Texture2D texture, Vector3 position) : base(game)
+        {
+            PO = new PositionedObject(game);
+            TheCamera = camera;
+            PlaneBasicEffect = new BasicEffect(game.GraphicsDevice);
+            Create(texture);
+            PO.Position = position;
+            game.Components.Add(this);
+        }
+
+        public PlaneEntity(Game game, Camera camera, Texture2D texture,
+            Vector3 position, Vector3 rotation) : base(game)
+        {
+            PO = new PositionedObject(game);
+            TheCamera = camera;
+            PlaneBasicEffect = new BasicEffect(game.GraphicsDevice);
+            Create(texture);
+            PO.Position = position;
+            PO.Rotation = rotation;
+            game.Components.Add(this);
         }
 
         public override void Initialize()
         {
             base.Initialize();
+            LoadContent();
+            BegenRun();
         }
 
-        public override void BeginRun()
+        protected override void LoadContent()
         {
-            base.BeginRun();
+            base.LoadContent();
+        }
+
+        public virtual void BegenRun()
+        {
 
         }
 
@@ -47,28 +81,27 @@ namespace Panther
         {
             base.Update(gameTime);
 
-            BaseWorld = Matrix.Identity;
+            BaseWorld = Matrix.CreateScale(PO.Scale) * RotateMatrix(PO.Rotation) *
+                Matrix.CreateTranslation(PO.Position);
 
-            BaseWorld = Matrix.CreateScale(Scale)
-                * Matrix.CreateFromYawPitchRoll(Rotation.Y, Rotation.X, Rotation.Z + -MathHelper.PiOver2);
-
-            if (Child)
+            if (PO.Child)
             {
-                BaseWorld *= Matrix.CreateTranslation(ParentPO.Position)
-                 * Matrix.CreateFromYawPitchRoll(ParentPO.Rotation.Y,
-                 ParentPO.Rotation.X, ParentPO.Rotation.Z)
-                 * Matrix.CreateTranslation(Position);
+                foreach (PositionedObject parentPO in PO.ParentPOs)
+                {
+                    BaseWorld *= Matrix.CreateTranslation(parentPO.Position)
+                     * RotateMatrix(parentPO.Rotation)
+                     * Matrix.CreateTranslation(parentPO.Position);
+                }
             }
+        }
 
+        public override void Draw(GameTime gameTime)
+        {
             // Set object and camera info
             PlaneBasicEffect.World = BaseWorld;
             PlaneBasicEffect.View = TheCamera.View;
             PlaneBasicEffect.Projection = TheCamera.Projection;
             Game.GraphicsDevice.SetVertexBuffer(PlaneVertexBuffer);
-        }
-
-        public void Draw()
-        {
             // Begin effect and draw for each frame
             foreach (EffectPass pass in PlaneBasicEffect.CurrentTechnique.Passes)
             {
@@ -76,6 +109,11 @@ namespace Panther
                 Game.GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(
                     PrimitiveType.TriangleList, Verts, 0, 2);
             }
+        }
+
+        public Matrix RotateMatrix(Vector3 rotation)
+        {
+            return Matrix.CreateFromYawPitchRoll(rotation.Y, rotation.X, rotation.Z);
         }
 
         public void Create(Texture2D texture)
@@ -99,7 +137,7 @@ namespace Panther
 
         public void ChangePlaneSize(float Width, float Height)
         {
-            SetupPlane(Width, Height);
+            SetupPlaneVertexBuffer(Width, Height);
         }
 
         public Texture2D Load(string textureName)
@@ -107,18 +145,18 @@ namespace Panther
             return Helper.LoadTexture(textureName);
         }
 
-        void SetupPlane(float width, float height)
+        void SetupPlaneVertexBuffer(float width, float height)
         {
             // Setup plane
             Width = width;
             Height = height;
 
-            Verts[0] = new VertexPositionTexture(new Vector3(-width / 3f, -height / 3f, 0), new Vector2(0, 0));
-            Verts[1] = new VertexPositionTexture(new Vector3(-width / 3f, height / 3f, 0), new Vector2(0, 1));
-            Verts[2] = new VertexPositionTexture(new Vector3(width / 3f, -height / 3f, 0), new Vector2(1, 0));
-            Verts[3] = new VertexPositionTexture(new Vector3(-width / 3f, height / 3f, 0), new Vector2(0, 1));
-            Verts[4] = new VertexPositionTexture(new Vector3(width / 3f, height / 3f, 0), new Vector2(1, 1));
-            Verts[5] = new VertexPositionTexture(new Vector3(width / 3f, -height / 3f, 0), new Vector2(1, 0));
+            Verts[0] = new VertexPositionTexture(new Vector3(-width / 2, -height / 2, 0), new Vector2(0, 0));
+            Verts[1] = new VertexPositionTexture(new Vector3(-width / 2, height / 2, 0), new Vector2(0, 1));
+            Verts[2] = new VertexPositionTexture(new Vector3(width / 2, -height / 2, 0), new Vector2(1, 0));
+            Verts[3] = new VertexPositionTexture(new Vector3(-width / 2, height / 2, 0), new Vector2(0, 1));
+            Verts[4] = new VertexPositionTexture(new Vector3(width / 2, height / 2, 0), new Vector2(1, 1));
+            Verts[5] = new VertexPositionTexture(new Vector3(width / 2, -height / 2, 0), new Vector2(1, 0));
 
             PlaneVertexBuffer.SetData(Verts);
         }
