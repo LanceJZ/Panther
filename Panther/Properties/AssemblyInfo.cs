4308ab38163fadcc5f333194fdf96463176e02cd
Lance Zimmerman <lance.xna@gmail.com>
2019-06-16 09:40:27 -0700

    Changed importer for obj models to FBX. Fixed ignore so that obj model files would be included.

--- Panther/Properties/AssemblyInfo.cs
+++ Panther/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.217.10.0")]
-[assembly: AssemblyFileVersion("0210.41.2.2359")]
+[assembly: AssemblyVersion("1.218.2.0")]
+[assembly: AssemblyFileVersion("1002.275.10.0427")]
